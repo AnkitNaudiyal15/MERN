5fff2217a312fbe0a162e4a2cacc7ebf0caaf950
Ankit naudiyal <ankit.naudiyal15@gmail.com>
2020-03-12 23:07:05 +0530

    first commit for file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import {BrowserRouter as Router,Route,Link} from "react-router-dom";
+import logo from "./logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AboutUs from "./components/about-us.component"; 
+import VehicleType from "./components/vehicle-type.component";  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#" target="_blank">
+            <img src={logo} width="100" height="50" alt="Parking App"></img>
+          </a>
+          <Link to="/" className="navbar-brand">MERN Learning app</Link>
+          <div className="nav-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create-vechile-type" className="nav-link">
+                  Vechicle Type
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/about" className="nav-link">
+                  Contact-us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact />
+        <Route path="/about" exact component={AboutUs}/>
+        <Route path="/create-vechile-type" exact component={VehicleType}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/about-us.component.js
@@ -0,0 +1,10 @@
+import React ,{Component} from 'react';
+function AboutUS(){
+    return (
+            <div className="row">
+                THis is about us contain
+            </div>
+    );
+}
+  
+  export default AboutUS;
